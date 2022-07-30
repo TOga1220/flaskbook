462ff61a188c021a594c118a13bf02e9a8f4dbbe
ogawa <birdchild84@gmail.com>
2022-07-30 18:37:32 +0900

    1.2章まで実行

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from unicodedata import name
+
+from flask import Flask, current_app, g, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -10,12 +12,21 @@ def index():
 def hello(name):
     return f"Hello, {name}!"
 
-# @app.route("/name/<name>")
-# def show_name(name):
-#     return render_template("index.html", name=name)
+@app.route("/name/<name>")
+def show_name(name):
+    return render_template("index.html", name=name)
 
-with app.test_request_context():
-    print(url_for("index"))
-    # print(url_for("show_name"))
-
+with app.test_request_context("/users?updated=1"):
+    print(request.args.get("updated"))
+    # print(url_for("index"))
+    # print(url_for("show_name", name="ichiro", page="1"))
     
+
+# アプリケーションコンテキスト
+# アプリケーションコンテキストを取得してスタックへpush
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセス可能
+print(current_app.name)
+
